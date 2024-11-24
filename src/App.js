37f19648e1b6299dfd624b0ff927b92d0d336ab9
tinamorlock <tinamorlock@gmail.com>
2024-11-23 20:37:37 -0600

    worked with shallow merge, setting state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            name: 'Tina Marie',
+            company: 'VersionCollab'
+        }
+    }
+  render() {
+    return (
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              {this.state.name} |  {this.state.company}
+            </p>
+              <button onClick={() => {
+                  this.setState( (state, props) => {
+                      return {
+                          name: 'xTina',
+                      }
+                  }, () => {
+                      console.log(this.state);
+                  } );
+              }}
+                  >
+                  Change Name
+              </button>
+          </header>
+        </div>
+    );
+  }
 }
 
 export default App;
